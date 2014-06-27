8a983fd6b826e139bf1c1a92aa29ac2640ac1c6f
Izuru Matsuura <iduru.matuura@gmail.com>
2014-06-27 23:42:32 +0900

    テンプレ追加

---
+++ Template.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long Long;
+#define whole(xs) xs.begin(), xs.end()
